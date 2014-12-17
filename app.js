44dd6cd7f39253411547b89b5fa5ea247a0b4a30
oberon <st900278@gmail.com>
2014-12-17 22:01:13 +0800

    test

---
+++ app.js
@@ -0,0 +1,43 @@
+var blocks = document.querySelectorAll("div.block");
+
+for (var i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener('click', function () {
+            var create = document.createElement("div");
+            document.querySelector("div.circle").parentNode.removeChild(document.querySelector("div.circle"));
+            create.className = this.className;
+            var xhr = new XMLHttpRequest();
+            this.style.position = "absolute";
+            this.style.zIndex = "2";
+            this.style.width = "100%";
+            this.style.height = "100%";
+            switch (this.className) {
+            case "block purple-bg":
+                this.className = "question-bg";
+                xhr.open("GET", "noise.html");
+                break;
+            case "block yellow-bg":
+                this.className = "alert-bg";
+                this.style.right = "0px";
+                break;
+            case "block blue-bg":
+                this.className = "info-bg";
+                this.style.bottom = "0px";
+                break;
+            case "block pink-bg":
+                this.className = "error-bg";
+                this.style.bottom = "0px";
+                this.style.right = "0px";
+                break;
+            }
+            xhr.send("");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    this.innerHTML = xhr.responseText;
+                    console.log(xhr.responseText);
+                }
+            }
+        
+
+            this.parentNode.insertBefore(create, this);
+    });
+}
