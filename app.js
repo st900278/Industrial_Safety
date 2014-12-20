0788759bcedb9d5d1aca9784dd33374745dcd521
oberon <st900278@gmail.com>
2014-12-20 23:32:16 +0800

    vibration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var blocks = document.querySelectorAll("div.block");
 for (var i = 0; i < blocks.length; i++) {
     blocks[i].addEventListener('click', function () {
         var create = document.createElement("div");
-        // document.querySelector("div.circle").parentNode.removeChild(document.querySelector("div.circle"));
+        document.querySelector("div.circle").parentNode.removeChild(document.querySelector("div.circle"));
         create.className = this.className;
         var xhr = new XMLHttpRequest();
         this.style.position = "absolute";
@@ -11,20 +11,20 @@ for (var i = 0; i < blocks.length; i++) {
         this.style.width = "100%";
         this.style.height = "100%";
         switch (this.className) {
-        case "block purple-bg":
+        case "animated block purple-bg bounceInLeft":
             this.className = "main question-bg";
             xhr.open("GET", "noise.html");
             break;
-        case "block yellow-bg":
+        case "animated block yellow-bg bounceInUp":
             this.className = "main alert-bg";
             this.style.right = "0px";
             xhr.open("GET", "vibration.html");
             break;
-        case "block blue-bg":
+        case "animated block blue-bg bounceInDown":
             this.className = "main info-bg";
             this.style.bottom = "0px";
             break;
-        case "block pink-bg":
+        case "animated block pink-bg bounceInRight":
             this.className = "main error-bg";
             this.style.bottom = "0px";
             this.style.right = "0px";
@@ -34,6 +34,7 @@ for (var i = 0; i < blocks.length; i++) {
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 document.querySelector("div.main").innerHTML = xhr.responseText;
+                eval(document.querySelector("div.main script").innerHTML);
             }
         }
         this.parentNode.insertBefore(create, this);
